c93a74431f67ff54e040d3ea0cd0228ff2c4a5e2
Fenhl <fenhl@fenhl.net>
2025-08-05 23:46:53 +0000

    Support nix-darwin

--- flake.nix
+++ flake.nix
@@ -6,7 +6,9 @@
     };
     outputs = { self, nixpkgs }: let
         supportedSystems = [
+            "aarch64-darwin"
             "aarch64-linux"
+            "x86_64-darwin"
             "x86_64-linux"
         ];
         forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
