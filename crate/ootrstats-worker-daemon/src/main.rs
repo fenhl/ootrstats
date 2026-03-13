df678e766a1901874084a8e1a46ec1dd52c2c258
Fenhl <fenhl@fenhl.net>
2026-03-13 01:53:33 +0000

    Upgrade rand dependency; Make Night report in worker daemon optional

--- crate/ootrstats-worker-daemon/src/main.rs
+++ crate/ootrstats-worker-daemon/src/main.rs
@@ -5,11 +5,13 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[wheel::main(rocket)]
 async fn main() -> Result<(), ootrstats_worker_daemon::MainError> {
-    let default_panic_hook = std::panic::take_hook();
-    std::panic::set_hook(Box::new(move |info| {
-        let _ = wheel::night_report_sync("/net/ootrstats/error", Some("thread panic"));
-        default_panic_hook(info)
-    }));
+    #[cfg(feature = "night")] {
+        let default_panic_hook = std::panic::take_hook();
+        std::panic::set_hook(Box::new(move |info| {
+            let _ = wheel::night_report_sync("/net/ootrstats/error", Some("thread panic"));
+            default_panic_hook(info)
+        }));
+    }
     ootrstats_worker_daemon::rocket().await?.launch().await?;
     Ok(())
 }
